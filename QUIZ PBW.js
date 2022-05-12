de8076c29b037b9a076b0215c1d3372cd67d9087
NAUFAL ILHAM FATIKH <90025282+naufalilhamfatikh@users.noreply.github.com>
2022-05-12 22:49:51 +0700

    Add files via upload

---
+++ QUIZ PBW.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+  $("#lipat").click(function(){
+    $("#tampil").slideDown("slow");
+  });
+});
+
+$(document).ready(function(){
+  $("#lipat2").click(function(){
+    $("#tampil2").slideDown("slow");
+  });
+});
+
+$(document).ready(function(){
+  $("#lipat3").click(function(){
+    $("#tampil3").slideDown("slow");
+  });
+});
+
+$(document).ready(function(){
+  $("#lipat4").click(function(){
+    $("#tampil4").slideDown(1000);
+  });
+});
+
+$(document).ready(function(){
+    $("#mulai1").click(function(){
+        $("#div1").fadeIn("slow");
+    });
+});
+
+$(document).ready(function(){
+    $("#mulai2").click(function(){
+        $("#div2").fadeIn(1000);
+    });
+});
+
+$(document).ready(function(){
+    $("#mulai3").click(function(){
+        $("#div3").fadeIn(1000);
+    });
+});
+
+$(document).ready(function(){
+    $("#mulai4").click(function(){
+        $("#div4").fadeIn(1000);
+    });
+});
+
